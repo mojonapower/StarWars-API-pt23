a82e91f25976a947b95782438ec4569f6a1ee2d2
mojonapower <maca95@gmail.com>
2022-04-20 01:27:17 +0000

    holi

--- src/main.py
+++ src/main.py
@@ -8,10 +8,11 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People
 #from models import Person
 
-app = Flask(__name__)
+#elementos de configuración 
+app = Flask(__name__) #instanciar una aplicación Flask
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -38,6 +39,27 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+#nuestras rutas
+@app.route('/people', methods=['GET'])
+def getPeople():
+    all_people = People.query.all() ##retorna un arreglo de clases
+    
+    arreglo_people = list(map(lambda x:x.serialize(), all_people))#me quedo solo con el resultado del metodo serialize
+
+    return jsonify({"Resultados": arreglo_people})
+
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def getPeopleID(people_id):
+    one_people = People.query.get(people_id)#buscar por id
+    if one_people:
+        return jsonify({"personaje": one_people.serialize()})
+    else:
+        return "error, te equivocaste :("
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -8,12 +8,30 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+#pipenv run migrate 
+#pipenv run upgrade
+#-> efectuar cambios en la base de datos
+#mysql;  use example; show tables; exit;
+
+class People(db.Model):
+    uid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(20), unique=False, nullable=True)
+    height = db.Column(db.Integer, unique=False, nullable=True)
+    mass = db.Column(db.Float, unique=False, nullable=True)
+
+    def serialize(self):
+        return {
+            "uid": self.uid,
+            "name": self.name,
+            "gender": self.gender,
+            "height": self.height,
+            "mass": self.mass
             # do not serialize the password, its a security breach
         }
